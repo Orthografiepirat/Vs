a7e304870a41fb6c515b57bebc0bd5089c470b86
Sven <orthografiepirat@tutanota.com>
2020-08-10 20:00:45 +0200

    First

---
+++ CookyBooky/Script/cookybooky.js
@@ -0,0 +1,25 @@
+"use strict";
+
+
+/** Handle GUI events */
+let handlerGUI = {
+    setVisibleSection : function(whatSection){
+        if(whatSection === "createNew"){
+            document.getElementById("sectionCreate").style.display = "block";
+            document.getElementById("sectionListSingle").style.display = "none";
+            document.getElementById("sectionListAll").style.display = "none";
+        } else if(whatSection === "displaySingle"){
+            document.getElementById("sectionCreate").style.display = "none";
+            document.getElementById("sectionListSingle").style.display = "block";
+            document.getElementById("sectionListAll").style.display = "none";
+        } else if(whatSection === "displayAll"){
+            document.getElementById("sectionCreate").style.display = "none";
+            document.getElementById("sectionListSingle").style.display = "none";
+            document.getElementById("sectionListAll").style.display = "block";
+        } else{
+            alert("Kein gültiger Wert");
+        }
+    }
+}
+
+/** Create HTML elements */
